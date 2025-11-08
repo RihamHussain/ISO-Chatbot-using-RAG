acbcb666cad2a5295dc56e049e4eb43f7d6e0a8a
Riham_Hussain <t-riham.hussain@zewailcity.edu.eg>
2025-11-08 21:45:28 +0200

    Uploding configurations done using pydantic

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter
-import os
+from helpers.config import get_settings
 Base_Router = APIRouter(
     prefix="/api/v1",
     tags=["chatbot"]
@@ -7,8 +7,9 @@ Base_Router = APIRouter(
 
 @Base_Router.get("/")
 async def welcome():
-    app_name = os.getenv('APP_NAME')
-    app_version = os.getenv('APP_VERSION')
+    app_settings = get_settings()
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {
         "app_name": app_name, 
         "app_version": app_version
